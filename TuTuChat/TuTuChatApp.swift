1a5fa44dfe3169109643114e0a49384cf0d1c6db
brilliant751 <2350240@tongji.edu.cn>
2025-11-12 17:02:01 +0800

    Initial Commit

---
+++ TuTuChat/TuTuChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuTuChatApp.swift
+//  TuTuChat
+//
+//  Created by brilliant751 on 2025/11/12.
+//
+
+import SwiftUI
+
+@main
+struct TuTuChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
